a41a8bae1863dfcacfb3b2170f7f15439416ec73
NaomiReyes <nreyes@ciencias.unam.mx>
2019-09-08 23:32:21 -0500

    Ultimos cambios - Prcatica3

--- PersonalBlog/Home/views.py
+++ PersonalBlog/Home/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.views import View
 
 def index(request):
     contex={}
     template= 'Home/index.html'
     return render(request, template, contex)
+
+class Index(View):
+    context={'title':"Titulo"}
+    template= 'Home/index.html'
+    def get(self, request):
+        return render(request, self.template, self.context)
+
+class About(View):
+    template = 'Home/about.html'
+    context = {'title': 'About me'}
+
+    def get(self, request):
+        return render(request, self.template, self.context)
 
